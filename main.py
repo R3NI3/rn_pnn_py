ad671e3f33bc3a98eb70f6fdfba493e17e5f36dd
diegothuran <diego.thuran@gmail.com>
2017-07-16 15:17:25 -0300

    Resultados

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-import pnn
+import PNN
 import Util
-import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_fscore_support
+import csv
 
 if __name__ == '__main__':
     dataset_names = ["digits", "diabets", "iris", "b_cancer"]
     results = {}
+    sigma_values = range(10)
     for name in dataset_names:
-        dataset = Util.load_datasets(name=name)
-        r = pnn.run(dataset.data.tolist(), dataset.target.tolist())
-        results[name] = [r[0], r[1][0], r[1][1], r[1][2], r[1][3]]
+        results[name] = []
+        for value in sigma_values:
+            pnn = PNN.PNN((value + 1)/10)
+            dataset = Util.load_datasets(name=name)
+            r = pnn.run(dataset.data.tolist(), dataset.target.tolist())
+            results[name].append([" ",r[0], r[1][0], r[1][1], r[1][2], r[1][3]])
 
-
-    print results
+    with open('results.csv', 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for name in dataset_names:
+            spamwriter.writerow([name, 'accuracy', 'Precision', "Recall", "F-Score", "Support"])
+            for key in results.keys():
+                if key == name:
+                    data = results[key]
+                    for line in data:
+                        spamwriter.writerow(line)
