a63bea3dc7d5235cee39f3ce366a05194e8b6e41
diegothuran <diego.thuran@gmail.com>
2017-07-17 13:14:47 -0300

    Resultados _ PCA

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ if __name__ == '__main__':
     results = {}
     sigma_values = range(10)
     for name in dataset_names:
-        print name
         results[name] = []
         for value in sigma_values:
-            print value
             value = float(value + 1)/10
-            pnn = PNN(sigma=value, fe_model=None)
+            pnn = PNN(sigma=value, fe_model='pca')
             dataset = Util.load_datasets(name=name)
             r = pnn.run(dataset.data.tolist(), dataset.target.tolist())
             results[name].append(["sigma: " + str(value), r[0], r[1][0], r[1][1], r[1][2], r[1][3]])
 
-    with open('results.csv', 'wb') as csvfile:
+    with open('results_pca.csv', 'wb') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for name in dataset_names:
             spamwriter.writerow([name, 'accuracy', 'Precision', "Recall", "F-Score", "Support"])
